52260ea091d1b29d68ec0b5b95efe160bdaf5a90
Richard Macklin <richard.github@nrm.com>
2020-03-31 23:55:25 -0700

    Fetch team data document from team-data-path input and log its contents
    The `fetchContent` function was copied from:
    https://github.com/actions/labeler/blob/d2c408e7ed8498dfdf675c5f8d133ab37b6f8520/src/main.ts#L88-L100

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,38 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    core.debug(new Date().toTimeString())
+    const token = core.getInput('repo-token', {required: true})
+    const teamDataPath = core.getInput('team-data-path')
 
-    core.setOutput('time', new Date().toTimeString())
+    const client = new github.GitHub(token)
+
+    core.debug(`Fetching team data from ${teamDataPath}`)
+    const teams: any = await getTeamData(client, teamDataPath)
+
+    core.debug(`teams: ${JSON.stringify(teams)}`)
   } catch (error) {
+    core.error(error)
     core.setFailed(error.message)
   }
+}
+
+async function getTeamData(client: github.GitHub, teamDataPath: string): Promise<any> {
+  const teamDataContent: string = await fetchContent(client, teamDataPath)
+
+  return JSON.parse(teamDataContent)
+}
+
+async function fetchContent(client: github.GitHub, repoPath: string): Promise<string> {
+  const response: any = await client.repos.getContents({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    path: repoPath,
+    ref: github.context.sha
+  })
+
+  return Buffer.from(response.data.content, response.data.encoding).toString()
 }
 
 run()
